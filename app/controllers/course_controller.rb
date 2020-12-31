a8f48397627cf25858e653b41a377f947bd9f54e
kousueke <k.koko.koko.kousuke@gmail.com>
2020-12-31 18:03:48 +0900

    コントローラーの記述

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,2 +1,7 @@
 class CourseController < ApplicationController
+  def index
+  end
+  
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "foods#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :course, only: [:index, :create]
 end
