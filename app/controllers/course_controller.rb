a42e3ea35193689139e5648755b64a92174a5d9e
kousueke <k.koko.koko.kousuke@gmail.com>
2021-01-13 12:48:52 +0900

    Rubocopの修正

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,5 +1,4 @@
 class CourseController < ApplicationController
-
   def new
     @course = Course.new
   end
@@ -15,7 +14,7 @@ class CourseController < ApplicationController
 
   def course_params
     params.require(:course).permit(
-      :body_weight, :target_value, :course_name_id).merge(user_id: current_user.id)
+      :body_weight, :target_value, :course_name_id
+    ).merge(user_id: current_user.id)
   end
-
 end
