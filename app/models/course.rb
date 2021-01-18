32f8e0c3699c6f63c93f4dd0a32c2a5dab5bf668
kousueke <k.koko.koko.kousuke@gmail.com>
2021-01-18 22:54:58 +0900

    コース・目標値の保存

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,8 @@
 class Course < ApplicationRecord
   belongs_to :user
-  belongs_to :course_name
+  # belongs_to :course_name
   validates :weight, numericality: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :course_name
 end
