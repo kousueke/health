40e972a822d74a34cf4febedec6be1386c50db5b
kousueke <k.koko.koko.kousuke@gmail.com>
2021-02-05 22:08:52 +0900

    ルーティングとコントローラーの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'foods/index'
   post 'foods/index'
 
+  get 'courses/index'
+  post 'courses/index'
+
   devise_for :users
   root to: "foods#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
