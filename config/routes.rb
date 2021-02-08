cee22dbe251eaac9931bc88434d67e688cad2438
kousueke <k.koko.koko.kousuke@gmail.com>
2021-02-08 23:16:17 +0900

    最初の画面の変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-  root to: "foods#index"
+  root to: "courses#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :foods, only: [:index, :new, :create]
   resources :courses, only: [:index, :new, :create]
