e45941ea245a704aee1008cdfbe80199754aff55
kousueke <k.koko.koko.kousuke@gmail.com>
2021-02-09 22:24:03 +0900

    登録者本人のみの食品表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-  root to: "courses#index"
+  root to: "foods#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :foods, only: [:index, :new, :create]
   resources :courses, only: [:index, :new, :create]
