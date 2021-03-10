25e6051afe9402def6fa107b8b2045251b6eed24
kousueke <k.koko.koko.kousuke@gmail.com>
2021-03-10 22:09:53 +0900

    コース画面の整理

--- app/models/course_name.rb
+++ app/models/course_name.rb
@@ -1,6 +1,6 @@
 class CourseName < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
+    { id: 1, name: 'コースを選択してください' },
     { id: 1.2, name: '健康コース' },
     { id: 1.6, name: 'マッチョコース' },
     { id: 2, name: '超マッチョコース' }
