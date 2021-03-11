05284abb4a5f7c56ca865c071f5e9ecc07786bfd
kousueke <k.koko.koko.kousuke@gmail.com>
2021-03-12 00:09:14 +0900

    フードコントローラーのコード整理

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,15 +1,12 @@
 class FoodsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_1, only: [:edit, :update, :destroy]
-  # before_action :set_2
-  # before_action :set_food3, only: [:index]
 
   def index
-    # //コースがなければ選択画面へ遷移する設定
-
+    # //コースがなければコース選択画面へ遷移する設定
     @foods = Food.all.includes(:user).order('created_at DESC')
-    @course = Course.all
     @user = User.all
+    @course = Course.all
   end
 
   def new
@@ -41,23 +38,6 @@ class FoodsController < ApplicationController
     redirect_to root_path
   end
 
-  # def set
-  #   redirect_to new_user_registration unless current_user.id == @course.user_id
-  # end
-
-  # def set_2
-  #   redirect_to courses_path @course.courses_id=nil
-  # end
-
-  # def set_food3
-  #   if user_signed_in? && @course.course_name_id == nil
-  #     redirect_to courses_path
-  #   end
-
-  # def set_food3
-  #   @course = Course.find(params[:id])
-  #   redirect_to courses_path if current_user.id == @course.id
-  # end
 
   private
 
